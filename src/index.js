b5439991a4899d01bdfaf9ef85fb2b0c52aa7ef8
inord777dev <inord777dev@gmail.com>
2021-08-02 22:31:19 +0300

    init: create task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let revers = n.toString().split('');
+    if (revers[0] === '-') {
+        revers.shift()
+    }
+    return revers.reverse().join('')
 }
